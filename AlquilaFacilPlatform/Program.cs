8db87cb5e9133cd93a978e85822afcb509f8f62f
angel <angelcca2001@gmail.com>
2024-06-06 01:51:48 -0500

    feat: added initial service configuration

--- AlquilaFacilPlatform/Program.cs
+++ AlquilaFacilPlatform/Program.cs
@@ -1,13 +1,80 @@
+using AlquilaFacilPlatform.Shared.Domain.Repositories;
+using AlquilaFacilPlatform.Shared.Infrastructure.Interfaces.ASP.Configuration;
+using AlquilaFacilPlatform.Shared.Infrastructure.Persistence.EFC.Configuration;
+using AlquilaFacilPlatform.Shared.Infrastructure.Persistence.EFC.Repositories;
+using AlquilaFacilPlatform.Subscriptions.Application.Internal.CommandServices;
+using AlquilaFacilPlatform.Subscriptions.Application.Internal.QueryServices;
+using AlquilaFacilPlatform.Subscriptions.Domain.Repositories;
+using AlquilaFacilPlatform.Subscriptions.Domain.Services;
+using AlquilaFacilPlatform.Subscriptions.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers( options => options.Conventions.Add(new KebabCaseRouteNamingConvention()));
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+    {
+        if (connectionString != null)
+            if (builder.Environment.IsDevelopment())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            else if (builder.Environment.IsProduction())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Error)
+                    .EnableDetailedErrors();    
+    });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1",
+            new OpenApiInfo
+            {
+                Title = "ACME.LearningCenterPlatform.API",
+                Version = "v1",
+                Description = "ACME Learning Center Platform API",
+                TermsOfService = new Uri("https://acme-learning.com/tos"),
+                Contact = new OpenApiContact
+                {
+                    Name = "ACME Studios",
+                    Email = "contact@acme.com"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Apache 2.0",
+                    Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+                }
+            });
+        c.EnableAnnotations();
+    });
+
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+builder.Services.AddScoped<ISubscriptionCommandService, SubscriptionCommandService>();
+builder.Services.AddScoped<ISubscriptionQueryServices, SubscriptionQueryService>();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
