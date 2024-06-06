f521faa1e9a64f5a281f25ffd23afa6ac4821568
angel <angelcca2001@gmail.com>
2024-06-06 01:50:56 -0500

    feat(subscriptions): added SubscriptionResourceFromEntityAssembler

---
+++ AlquilaFacilPlatform/Subscriptions/Interfaces/REST/Transform/SubscriptionResourceFromEntityAssembler.cs
@@ -0,0 +1,12 @@
+using AlquilaFacilPlatform.Subscriptions.Domain.Model.Aggregates;
+using AlquilaFacilPlatform.Subscriptions.Interfaces.REST.Resources;
+
+namespace AlquilaFacilPlatform.Subscriptions.Interfaces.REST.Transform;
+
+public static class SubscriptionResourceFromEntityAssembler
+{
+    public static SubscriptionResource ToResourceFromEntity(Subscription entity)
+    {
+        return new SubscriptionResource(entity.Id, entity.UserId, entity.PlanId);
+    }
+}
