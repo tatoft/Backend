4c145264cceec4aa039287cf9d46b77b8d71a261
angel <angelcca2001@gmail.com>
2024-06-06 01:38:40 -0500

    feat(subscription): added aggregate Subscription

---
+++ AlquilaFacilPlatform/Subscriptions/Domain/Model/Aggregates/Subscription.cs
@@ -0,0 +1,33 @@
+using AlquilaFacilPlatform.Subscriptions.Domain.Model.Commands;
+
+namespace AlquilaFacilPlatform.Subscriptions.Domain.Model.Aggregates;
+
+public partial class Subscription
+{
+    public int Id { get; }
+    
+    public int UserId { get; private set; }
+    public int PlanId { get; private set; }
+
+    public Subscription()
+    {
+        UserId = 0;
+        PlanId = 0;
+    }
+
+    public Subscription(int userId, int planId)
+    {
+        UserId = userId;
+        PlanId = planId;
+    }
+
+    public Subscription(CreateSubscriptionCommand command)
+    {
+        UserId = command.UserId;
+        PlanId = command.PlanId;
+    }
+
+    public int getUserId => UserId;
+    public int getPlanId => PlanId;
+
+}
