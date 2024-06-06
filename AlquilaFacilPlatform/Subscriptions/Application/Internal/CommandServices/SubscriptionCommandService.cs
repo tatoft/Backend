dc6a15e8617990149cc4bcaf1fbfe7a680a409a7
angel <angelcca2001@gmail.com>
2024-06-06 01:49:17 -0500

    feat(subscriptions): added SubscriptionCommandService

--- AlquilaFacilPlatform/Subscriptions/Application/Internal/CommandServices/SubscriptionCommandService.cs
+++ AlquilaFacilPlatform/Subscriptions/Application/Internal/CommandServices/SubscriptionCommandService.cs
@@ -1,15 +1,19 @@
-/*namespace AlquilaFacilPlatform.Subscriptions.Application.Internal.CommandServices;
-/*
-public class SubscriptionCommandService(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
-    : ICategoryCommandService
+using AlquilaFacilPlatform.Shared.Domain.Repositories;
+using AlquilaFacilPlatform.Subscriptions.Domain.Model.Aggregates;
+using AlquilaFacilPlatform.Subscriptions.Domain.Model.Commands;
+using AlquilaFacilPlatform.Subscriptions.Domain.Repositories;
+using AlquilaFacilPlatform.Subscriptions.Domain.Services;
+
+namespace AlquilaFacilPlatform.Subscriptions.Application.Internal.CommandServices;
+
+public class SubscriptionCommandService(ISubscriptionRepository subscriptionRepository, IUnitOfWork unitOfWork)
+    : ISubscriptionCommandService
 {
     public async Task<Subscription?> Handle(CreateSubscriptionCommand command)
     {
-        var category = new Subscription(command.Name);
-        await categoryRepository.AddAsync(category);
+        var subscription = new Subscription(command.UserId, command.PlanId);
+        await subscriptionRepository.AddAsync(subscription);
         await unitOfWork.CompleteAsync();
-        return category;
+        return subscription;
     }
 }
-
-*/
