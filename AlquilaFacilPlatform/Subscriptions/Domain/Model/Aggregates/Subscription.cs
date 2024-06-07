91b8165810dab3d30404a21173e7d1b0998f7a8c
angel <angelcca2001@gmail.com>
2024-06-07 06:05:19 -0500

    feat: added subscription status to Subscription aggregate

--- AlquilaFacilPlatform/Subscriptions/Domain/Model/Aggregates/Subscription.cs
+++ AlquilaFacilPlatform/Subscriptions/Domain/Model/Aggregates/Subscription.cs
@@ -1,4 +1,5 @@
 using AlquilaFacilPlatform.Subscriptions.Domain.Model.Entities;
+using AlquilaFacilPlatform.Subscriptions.Domain.Model.ValueObjects;
 
 namespace AlquilaFacilPlatform.Subscriptions.Domain.Model.Aggregates;
 
@@ -7,6 +8,10 @@ public partial class Subscription
     public int Id { get; }
     public int UserId { get; private set; }
     
+    public ESubscriptionStatus Status { get; protected set; }
+    
+    public ICollection<Invoice> Invoices { get; }
+    
     public Plan Plan { get; internal set; }
     public int PlanId { get; private set; }
 
@@ -14,6 +19,19 @@ public partial class Subscription
     {
         UserId = userId;
         PlanId = planId;
+        Status = ESubscriptionStatus.Pending;
     }
 
+    public void Active() => Status = ESubscriptionStatus.Active;
+    public void Pending() => Status = ESubscriptionStatus.Pending;
+    public void Expired() => Status = ESubscriptionStatus.Expired;
+    public void Cancelled() => Status = ESubscriptionStatus.Cancelled;
+    public void Suspended() => Status = ESubscriptionStatus.Suspended;
+    public void Trial() => Status = ESubscriptionStatus.Trial;
+    public void RenewalDue() => Status = ESubscriptionStatus.RenewalDue;
+
+    public ESubscriptionStatus GetStatus()
+    {
+        return Status;
+    }
 }
