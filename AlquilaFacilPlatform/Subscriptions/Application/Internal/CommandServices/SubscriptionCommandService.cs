3c0aa22a50a64ef36d602331b28b363724aeff6c
angel <angelcca2001@gmail.com>
2024-06-07 06:07:10 -0500

    feat: added assignment of plan to subscription in SubscriptionCommandService

--- AlquilaFacilPlatform/Subscriptions/Application/Internal/CommandServices/SubscriptionCommandService.cs
+++ AlquilaFacilPlatform/Subscriptions/Application/Internal/CommandServices/SubscriptionCommandService.cs
@@ -1,10 +1,8 @@
 using AlquilaFacilPlatform.Shared.Domain.Repositories;
 using AlquilaFacilPlatform.Subscriptions.Domain.Model.Aggregates;
 using AlquilaFacilPlatform.Subscriptions.Domain.Model.Commands;
-using AlquilaFacilPlatform.Subscriptions.Domain.Model.Entities;
 using AlquilaFacilPlatform.Subscriptions.Domain.Repositories;
 using AlquilaFacilPlatform.Subscriptions.Domain.Services;
-using AlquilaFacilPlatform.Subscriptions.Infrastructure.Persistence.EFC.Repositories;
 
 namespace AlquilaFacilPlatform.Subscriptions.Application.Internal.CommandServices;
 
